532d0de983664e66495ab88c4c88337a4bbec48f
MBHallgren <s153002@student.dtu.dk>
2023-08-22 10:16:06 +0200

    test5

--- conda/make_conda.py
+++ conda/make_conda.py
@@ -1,55 +1,43 @@
-import yaml
-import os
-import sys
+import ruamel.yaml as yaml
 
-sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)), '')] + sys.path
+# Create an ordered dictionary for each section
+package = yaml.comments.CommentedMap()
+package['name'] = 'kma'
+package['version'] = '1.4.14'
 
-with open('version.h', 'r') as f:
-    for line in f:
-        if line.startswith('#define'):
-            version = line.split()[2].strip('"')
+source = yaml.comments.CommentedMap()
+source['url'] = 'https://bitbucket.org/genomicepidemiology/kma/get/1.4.14.tar.gz'
 
+build = yaml.comments.CommentedMap()
+build['number'] = 0
 
-data = {
-    "package": {
-        "name": "kma",
-        "version": version
-    },
-    "source": {
-        "url": "https://bitbucket.org/genomicepidemiology/kma/get/{}.tar.gz".format(version),
-    },
-    "build": {
-        "number": 0
-    },
-    "requirements": {
-        "build": [
-            "make",
-            "{{ compiler('c') }}"
-        ],
-        "host": [
-            "zlib"
-        ],
-        "run": [
-            "zlib"
-        ]
-    },
-    "about": {
-        "home": "https://bitbucket.org/genomicepidemiology/kma",
-        "summary": "KMA is mapping a method designed to map raw reads directly against redundant databases, in an ultra-fast manner using seed and extend.",
-        "license": "Apache-2.0"
-    },
-    "extra": {
-        "identifiers": {
-            "doi": "10.1186/s12859-018-2336-6",
-        }
-    }
-}
+requirements = yaml.comments.CommentedMap()
+requirements['build'] = ['make', '{{ compiler(\'c\') }}']
+requirements['host'] = ['zlib']
+requirements['run'] = ['zlib']
 
-# Convert the data to YAML and print it
-yaml_str = yaml.dump(data, sort_keys=False).replace("'{{", "{{").replace("}}'", "}}").replace("''", "'")
+about = yaml.comments.CommentedMap()
+about['home'] = 'https://bitbucket.org/genomicepidemiology/kma'
+about['summary'] = 'KMA is mapping a method designed to map raw reads directly against redundant databases, in an ultra-fast manner using seed and extend.'
+about['license'] = 'Apache-2.0'
 
-print (yaml_str)
+extra = yaml.comments.CommentedMap()
+identifiers = yaml.comments.CommentedMap()
+identifiers['doi'] = '10.1186/s12859-018-2336-6'
+extra['identifiers'] = identifiers
+
+# Create a dictionary for the entire YAML content
+data = yaml.comments.CommentedMap()
+data['package'] = package
+data['source'] = source
+data['build'] = build
+data['requirements'] = requirements
+data['about'] = about
+data['extra'] = extra
+
+# Serialize the data to YAML and print it
+yaml_str = yaml.dump(data, Dumper=yaml.RoundTripDumper).replace("\"{{", "{{").replace("}}\"", "}}")
+print(yaml_str)
 
 with open('conda/meta.yaml', 'w') as f:
     f.write(yaml_str)
-
